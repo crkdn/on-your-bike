2f299bd543ab7428f55b2cd07683e07231e5d14f
crkdn@peterjh.com <crkdn@peterjh.com>
2018-03-15 11:53:43 +0000

    Create importable module for connecting to our database

--- on-your-bike/static_data.py
+++ on-your-bike/static_data.py
@@ -1,15 +1,6 @@
 import requests
-import mysql.connector
+import db_connection as db
 
-# Keep keys outside of VCS
-with open("credentials/dbinfo.txt") as dbinfo:
-    db_creds = dbinfo.read().splitlines()
-    db_hostname = db_creds[0]
-    db_user = db_creds[1]
-    db_pass = db_creds[2]
-
-cnx = mysql.connector.connect(host=db_hostname, user=db_user, password=db_pass, database="OnYourBikeDB")
-cursor = cnx.cursor(buffered=True)
 query = "INSERT INTO BikeStationStaticData VALUES (%s, %s, %s, %s, %s)"
 
 all_stations = requests.get("https://developer.jcdecaux.com/rest/vls/stations/Dublin.json").json()
@@ -20,8 +11,8 @@ for station in all_stations:
     latitude = station["latitude"]
     longitude = station["longitude"]
 
-    cursor.execute(query, (number, name, address, latitude, longitude))
+    db.cursor.execute(query, (number, name, address, latitude, longitude))
 
-cnx.commit()
+db.commit_and_close()
 
 print()
