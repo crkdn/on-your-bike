14d390379dfa426f59eda4b2886a515596d2b652
Peter H <crkdn@peterjh.com>
2018-04-01 23:20:02 +0100

    Update server to return coo-ordinates and current data

--- on-your-bike/app.py
+++ on-your-bike/app.py
@@ -1,13 +1,35 @@
-from flask import Flask
+from flask import Flask, jsonify
 from flask.templating import render_template
+import db_connection as db
 
-app = Flask (__name__)
+app = Flask(__name__)
+
 
 @app.route('/')
 def index():
     return render_template("index.html")
 
+
+@app.route('/all-locations')
+def get_station_static_data():
+    locations = []
+    db.cursor.execute("SELECT * FROM BikeStationStaticData")
+    for row in db.cursor:
+        locations.append({"number": row[0], "latitude": row[3], "longitude": row[4]})
+    return jsonify(locations)
+
+
+@app.route("/live-data/<station_id>")
+def get_live_data(station_id):
+    db.cursor.execute("SELECT Address, Timestamp, Status, BikeStands, AvailableStands FROM OnYourBikeDB.BikeStationStaticData, OnYourBikeDB.BikeStationDynamicData WHERE BikeStationStaticData.Number = BikeStationDynamicData.Number AND BikeStationStaticData.Number = %s ORDER BY Timestamp DESC LIMIT 1", (station_id, ))
+    for (address, timestamp, status, bike_stands, available_stands) in db.cursor:
+        return jsonify(
+            {"address": address,
+             "timestamp": timestamp,
+             "status": status,
+             "bikes": bike_stands - available_stands,
+             "available": available_stands})
+
+
 if __name__ == '__main__':
     app.run(debug=True)
-    
-    
