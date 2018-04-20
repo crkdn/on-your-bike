30e3b5d871fe9f252e0ee95810fb8732f05d6914
Peter H <crkdn@peterjh.com>
2018-04-20 17:24:00 +0100

    Add comments and minor code-restructuring

--- on-your-bike/dynamicweatherdata.py
+++ on-your-bike/dynamicweatherdata.py
@@ -1,29 +1,33 @@
+"""
+This module is run every 30 minutes to poll OpenWeatherMap's API for the current weather in Dublin.
+The data is then saved into our database, and will be used to train a prediction model.
+"""
 import requests
-import json
 import db_connection as db
 import os
 
+# cron runs in "/" by default. Change working directory to not break relative locations:
 working_dir = os.path.dirname(os.path.abspath(__file__))
 
 # Get OWM API key (do not place credentials under VCS)
 with open(os.path.join(working_dir, "credentials/WeatherMapAPIKey.txt")) as file:
     api_key = file.readline().strip()
 
-#id is the key, value is the key for Dublin
+# id is the key, value is the key for Dublin
 parameters = {"id": 2964574, "APPID": api_key}
 
 # response is the name of my dictionary
 response = requests.get("https://api.openweathermap.org/data/2.5/weather", params=parameters).json()
 
-# get the right data
-WeatherDescription= response["weather"][0]["main"]
+# get the right data, returning null if element not found
+WeatherDescription = response["weather"][0]["main"]
 Icon = response["weather"][0]["icon"]
-Temperature= (response.get("main") or {}).get("temp")
+Temperature = (response.get("main") or {}).get("temp")
 WindSpeed = (response.get("wind") or {}).get("speed")
 Clouds = (response.get("clouds") or {}).get("all")
 Rain = (response.get("rain") or {}).get("3h")
 Snow = (response.get("snow") or {}).get("3h")
-TimeStamp= response.get("dt")
+TimeStamp = response.get("dt")
 Sunrise = (response.get("sys") or {}).get("sunrise")
 Sunset = (response.get("sys") or {}).get("sunset")
 
--- on-your-bike/static/scripts/map.js
+++ on-your-bike/static/scripts/map.js
@@ -1,10 +1,13 @@
+// Wrapper to initialise both the Google Map element and
+// the Google Map-augmented search boxes at the same time:
 function initialise() {
     localStorage.removeItem("location");    // Delete saved location if user refreshes the page
     initMap();
     initAutocomplete();
 }
 
-let map;
+// Initialise the Google Map element
+let map;    // Need to access the map var in other functions, so we create it globally before using it in initMap
 function initMap() {
     console.log("Map init");
     map = new google.maps.Map(document.getElementById('map-container'), {
@@ -18,38 +21,44 @@ function initMap() {
     let markerCluster = drawMarkers(map);
 }
 
+// Initialise the location auto-completing search boxes
 function initAutocomplete() {
+    // Restrict search to Dublin area.
     const dublinBounds = new google.maps.LatLngBounds(
         new google.maps.LatLng(53.326093, -6.315996),
         new google.maps.LatLng(53.383076, -6.205615)
     );
 
+    // Options to pass to the search box constructors
     const options = {
         bounds: dublinBounds,
         strictBounds: true
     };
 
     let fromInput = document.getElementById("from-location");
-    fromAutocomplete = new google.maps.places.Autocomplete(fromInput, options); // This needs to NOT be explicitly declared as a variable or it fails.
+    fromAutocomplete = new google.maps.places.Autocomplete(fromInput, options); // This needs to NOT be explicitly declared as a variable or it fails. No idea why - ask Google!
 
     let toInput = document.getElementById("to-location");
-    toAutocomplete = new google.maps.places.Autocomplete(toInput, options);     // No idea why - ask Google!
+    toAutocomplete = new google.maps.places.Autocomplete(toInput, options);
 }
 
 const marker_list = [];
 function drawMarkers(map) {
+    // Talk to our Flask route to get a list of all DB station locations
     fetch("all-locations")
         .then(function (response){
-            return response.json()
+            return response.json()  // Convert response string into JSON object
             })
         .then(function (stationJSON){
-            console.log(stationJSON);
+            // Returns an array of all GMap marker elements (used by Map constructor to draw on Map)
             return stationJSON.map(function (station) {
                 let marker = new google.maps.Marker({
                     position: {lat: station["latitude"], lng: station["longitude"]},
                     label: station["number"].toString()
                     });
 
+                // While constructing Marker element, add an event listener so that we can
+                // fetch live data when the Marker is clicked.
                 marker.addListener("click", function(){
                     document.getElementById("current-data").innerHTML = "<i>Loading...</i>";
                     fetch(`live-data/${this.label}`).then(function(response){
@@ -59,18 +68,18 @@ function drawMarkers(map) {
                             twentyFourHourGraph(dynamicDataJSON);
                         });
                 });
-
                 marker_list.push(marker);
                 return marker;
             });
         })
         .then(function (markers){
-            return new MarkerClusterer(map, markers, {
+            return new MarkerClusterer(map, markers, {  // Use Google's MarkerClusterer library to keep map clean by grouping nearby markers together until zoomed in
                 imagePath: "../static/scripts/MarkerClusterer/m"
             });
         });
 }
 
+// Finds the three closest markers to a given co-ordinate pair
 // Approach taken from
 // https://stackoverflow.com/questions/7997627/google-maps-how-to-get-the-distance-between-two-point-in-metre/7997732#7997732
 function getNearestMarkers(markers, lat_long){
@@ -101,14 +110,15 @@ function getNearestMarkers(markers, lat_long){
     for(let i = 0;i < 3;i++){
         nearest_dict[nearest[i]] = dist_map[nearest[i]];
     }
-    console.log(nearest_dict);
     return nearest_dict;
 }
 
+// Used by getNearestMarkers
 function calculate_distance(p1,p2){
     return (google.maps.geometry.spherical.computeDistanceBetween(p1, p2) / 1000).toFixed(2);
 }
 
+// Replace our sidebar content with live data about a Station
 function currentData(singleJson){
     document.getElementById("current-data").innerHTML = `<h4 class="text-center">${singleJson["address"]}</h4>
     <hr>
@@ -132,11 +142,11 @@ function currentData(singleJson){
     <hr>`;
 }
 
+// Draw a graph of the number of bikes available at a given station
 // Reference : https://canvasjs.com/
 function twentyFourHourGraph(multiJson){
     const f_ar = [];
     multiJson.forEach(function(days){
-    
         let time = days["timestamp"];
         let available = days["available"];
 
@@ -151,8 +161,6 @@ function twentyFourHourGraph(multiJson){
         b_obj.x = time_conver(value_i);
         b_obj.y = available_i;
         f_ar.push(b_obj);
- 
-
     });
 
     let chart = new CanvasJS.Chart("graph-container",
@@ -185,16 +193,19 @@ function twentyFourHourGraph(multiJson){
                   dataPoints: f_ar
               }]
         });
-
     chart.render();
 }
 
+// Gets co-ordinate pairs for starting location and end location.
 function startToEnd() {
     let fromLat;
     let fromLng;
     let toLat;
     let toLng;
 
+    // If the search text-box is disabled, it means the "Use current location" checkbox
+    // is checked, so we are using raw latitude and longitude in a string, rather than
+    // a computed location from GMaps
     if (document.getElementById("from-location").disabled){
         let fromCoord = document.getElementById("from-location").value.split(", ");
         fromLat = parseFloat(fromCoord[0]);
@@ -213,24 +224,25 @@ function startToEnd() {
         toLng = toAutocomplete.getPlace().geometry.location.lng();
     }
 
-    document.getElementById("availability-header").innerHTML = "Plan Your Journey";
+    document.getElementById("availability-header").innerHTML = "Plan Your Journey"; // Visual cue that info is coming.
     threePredictions(fromLat, fromLng, "from-prediction");
     threePredictions(toLat, toLng, "to-prediction");
 }
 
+// Makes predictions about the three closest markers to a location
 function threePredictions(latitude, longitude, elementId){
-    console.log("MarkerList:");
-    console.log(marker_list);
-    let nearby = getNearestMarkers(marker_list, [latitude, longitude]);
-    let predictionHTML = [];
-    let startOrEnd = elementId === "from-prediction" ? "starting" : "end";
+    let nearby = getNearestMarkers(marker_list, [latitude, longitude]); // Three closest markers to user location
+    let predictionHTML = [];    // Empty array which will be filled with strings to display on page
+    let startOrEnd = elementId === "from-prediction" ? "starting" : "end";  // "at your starting point" || "at your end point"
+
     for (let marker in nearby){
-        console.log(nearby[marker][0]["label"]);
+        // Talk to our Flask route to get current weather info
         fetch("all-weather")
             .then(function(weatherResponse){
-                return weatherResponse.json();
+                return weatherResponse.json();  // Convert string to JS Object
             })
             .then(function(weatherJSON){
+                // Pass weather features, along with station ID and current day/time, as POST body to our prediction Route
                 fetch("station_prediction", {
                     method: "post",
                     headers: {
@@ -246,17 +258,20 @@ function threePredictions(latitude, longitude, elementId){
                         temperature: weatherJSON["temperature"],
                         windspeed: weatherJSON["wind"]
                     })
-                })
+                    })
                     .then(function(predictionResponse){
-                        return predictionResponse.json();
+                        return predictionResponse.json();   // Convert Prediction Response to Object
                     })
                     .then(function(predictionJSON){
+                        // Generate string based on prediction response and add it to predictionHTML array.
                         let stationNumber = nearby[marker][0]["label"];
                         predictionHTML.push(`<span class="station-zoom" onclick="zoomToMarker(${stationNumber})"><img src="https://maps.google.com/mapfiles/ms/icons/red.png" alt="marker-icon"><b>Station ${stationNumber}:</b></span><br>
 <i>Available bikes:</i> ${predictionJSON["bike_availability"]}<br>
 <i>Available stands:</i> ${predictionJSON["bike_stand_availability"]}`);
                     })
                     .then(function(){
+                        // If we have generated the same number of strings as the as the number of nearby
+                        // markers we received earlier, update the web page:
                         if (predictionHTML.length === Object.keys(nearby).length){
                             document.getElementById(elementId).innerHTML = `Stations near your ${startOrEnd} point for the next hour (click to see on the map):<hr> <br>` + predictionHTML.join("<br>\n") + "<br><br>";
                         }
@@ -265,8 +280,12 @@ function threePredictions(latitude, longitude, elementId){
     }
 }
 
+// Each station header in the prediction HTML has an onclick event which passes in a station id number (as a string),
+// to allow the user to click on a predicted station and see it on the map.
 function zoomToMarker(markerNumber){
     for (let marker in marker_list){
+        // Loop through the array of Marker objects to find the one
+        // with the same label as the number passed to this function
         if(markerNumber.toString() === marker_list[marker]["label"]){
             map.setZoom(18);
             map.panTo(marker_list[marker].position);
@@ -274,6 +293,7 @@ function zoomToMarker(markerNumber){
     }
 }
 
+// Uses the browser's built in geolocation to get lat & lng for the user's current position
 function getLocation(inputBoxId) {
     if (navigator.appVersion.includes("Chrome")){
         window.alert("Chrome requires HTTPS for this feature to work. If you wish to use this feature, please use another browser.");
@@ -289,16 +309,18 @@ function getLocation(inputBoxId) {
                 inputBoxElement.removeAttribute("disabled");
                 inputBoxElement.value = "";
             } else {
+                // The geolocation function didn't like it when the user tried to use it morethan once in quick succession
+                // Storing the result in localStorage allows us to retrieve it instantly in this case.
                 let storedLocation = localStorage.getItem("location");
                 if (storedLocation){
                     let locationJSON = JSON.parse(storedLocation);
-                    inputBoxElement.value = locationJSON["lat"] + ", " + locationJSON["lng"]
+                    inputBoxElement.value = locationJSON["lat"] + ", " + locationJSON["lng"];
                     inputBoxElement.disabled = "true";
                 } else if (navigator.geolocation){
                     navigator.geolocation.getCurrentPosition(function (position) {
                         let inputBoxElement = document.getElementById("from-location");
                         inputBoxElement.value = position.coords.latitude + ", " + position.coords.longitude;
-                        inputBoxElement.disabled = "true";
+                        inputBoxElement.disabled = "true"; // Stop user changing lat & lng accidentally
                         localStorage.setItem("location", JSON.stringify({lat: position.coords.latitude, lng: position.coords.longitude}));
                     });
                 }
@@ -335,7 +357,7 @@ function getLocation(inputBoxId) {
 			}
 		})
 		.then(function(JSONWeather){
-			// {"description": WeatherDescription, "icon": 04d, "temperature": Temperature}
+			// Replace weather placeholder div contents with live results.
 			return `<b>Current weather in Dublin:</b> ${Math.floor(JSONWeather["temperature"] - 273.15)}&deg;C - ${JSONWeather["description"]} <img src="http://openweathermap.org/img/w/${JSONWeather["icon"]}.png">`;
 		})
 		.then(function(HTMLString){
